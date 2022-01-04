38443222a157de79f0130a8c44e46da004e6cac9
Axelpvz2030 <89265207+Axelpvz2030@users.noreply.github.com>
2022-01-03 20:53:34 -0600

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,16 +1,16 @@
 #ifndef __FUNCIONES_H__
 #define __FUNCIONES_H__
 
-//Prototipos de las funciones
+//__________________PROTOTIPOS DE LAS FUNCIONES____________________
 
-//Funciones con sistema decimal
+//__________________FUNCIONES CON SISTEMA DECIMAL__________________
 int conv_bin_decimal(char *bin);
 char* conv_decimal_bin(int num);
 int hexa_decimal(char *hexa);
 char* decimal_hexa(int num);
 int oct_decimal(char *oct);
 char* decimal_oct(int num);
-//Funciones con sistema binario
+//___________________FUNCIONES CON SISTEMA BINARIO__________________
 char* bin_hexa(char *bin);
 char* hexa_bin(char *hexa);
 int hexa_bin2(int num);
